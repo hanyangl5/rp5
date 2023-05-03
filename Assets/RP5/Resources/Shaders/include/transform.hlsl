1fa1e3d1b481737671aad34a9d4b2ea962ca7f5e
hylu-laptop <hy.lu@outlook.com>
2023-05-03 18:27:22 +0800

    stash

--- Assets/RP5/Resources/Shaders/include/transform.hlsl
+++ Assets/RP5/Resources/Shaders/include/transform.hlsl
@@ -1,10 +1,19 @@
 #ifndef TRANSFORM_HLSL
 #define TRANSFORM_HLSL
 
-float4 ComputeWorldPosFromDepth(float2 uv, float4x4 inverse_vp, float depth) {
-    float4 pos_cs = float4(uv.x, uv.y, depth, 1.0);
+// uv is from [0, 1]
+float4 ComputePositionWsFromDepth(float2 uv, float4x4 inverse_vp, float depth) {
+    float4 pos_cs = float4(uv * 2.0 - 1.0, depth, 1.0);
     pos_cs.y = -pos_cs.y;
     float4 pos_ws = mul(inverse_vp, pos_cs);
+    pos_ws /= pos_ws.w;
+    return pos_ws;
+}
+
+float4 ComputePositionVsFromDepth(float2 uv, float4x4 inverse_p, float depth) {
+    float4 pos_cs = float4(uv * 2.0 - 1.0, depth, 1.0);
+    pos_cs.y = -pos_cs.y;
+    float4 pos_ws = mul(inverse_p, pos_cs);
     pos_ws /= pos_ws.w;
     return pos_ws;
 }
